f150f390aa9e5a2fee31dbcde56a8f1ab7866598
andreesantos <86435088+andreesantos@users.noreply.github.com>
2021-07-06 15:59:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function vslidaIdade(idade){
+    var validar;
+    if (idade >=18){
+
+        validae = true
+    }else{
+validar = false
+}
+
+}
+
+alert(soma(5, 10))
+
+
+
+
+/*
+var d = new Date();
+alert (d.getDay());
+alert (d.getHours());
+alert (d.getMinutes());
+*/
+
+/*
+var count;
+for( count*0; count < 5; count ++){
+    alert ( count );
+};
+*/
+/*
+
+
+
+
+var nome = "Andre Santos";
+var idade = 36;
+var idade2 = 10,;
+alert(nome + " tem " + idade + " anos ");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
